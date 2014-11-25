0081db01e1ee1a684ca00b74b966f6f610465c65
calebgeniesse <calebgeniesse@gmail.com>
2014-11-25 13:41:29 -0800

    Updating setup_stepwise_benchmark.py to determine VDW_align_res and full_align_res for VDW_rep_screen_info flag.

--- python/benchmark_util/setup_stepwise_benchmark.py
+++ python/benchmark_util/setup_stepwise_benchmark.py
@@ -264,9 +264,9 @@ for name in names:
                 screen_align_res.append( n )
                 working_align_res.append( get_fullmodel_number(working_reschain,working_pdb_info[4],working_pdb_info[3]) ) 
         if len( screen_align_res ): screen_align_res_tag = make_tag_with_dashes( screen_align_res )
-        else:   screen_align_res_tag = '-'
+        else:   screen_align_res_tag = '0-0'
         if len( working_align_res ):    working_align_res_tag = make_tag_with_dashes( working_align_res )
-        else:   working_align_res_tag = '-'
+        else:   working_align_res_tag = '0-0'
         return ( screen_align_res_tag, working_align_res_tag )
 
     prefix = '%s/%s_PERIPHERAL_REGIONS_' % ( inpath, name )
@@ -275,8 +275,11 @@ for name in names:
     ###6-44( align_res of VDW_rep_screen_pose ) 1-33( align_res of working_pose )
     working_fixed_res = input_res[ name ]
     ( VDW_align_res, full_align_res ) = get_align_res( VDW_rep_screen_pdb[ name ], working_native[ name ], working_fixed_res ) 
-    VDW_rep_screen_info[ name ] = '%s %s %s' % ( basename( VDW_rep_screen_pdb[name] ), VDW_align_res, full_align_res ) 
-    
+    if args.swa:
+        VDW_rep_screen_info[ name ] = '%s %s %s' % ( basename( VDW_rep_screen_pdb[name] ), VDW_align_res, full_align_res ) 
+    else:
+        VDW_rep_screen_info[ name ] = '%s' % ( basename( VDW_rep_screen_pdb[name] ) ) 
+
             
 if len (args.extra_flags) > 0:
     if exists( args.extra_flags ):
