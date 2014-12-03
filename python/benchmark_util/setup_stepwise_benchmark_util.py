0d855d7fdf26a53a17ca74e0202603049d2ada78
calebgeniesse <calebgeniesse@gmail.com>
2014-12-03 11:41:29 -0800

    Moving helper functions used in setup_stepwise_benchmark.py into setup_stepwise_benchmark_util.py

---
+++ python/benchmark_util/setup_stepwise_benchmark_util.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+#####################################################################################################################
+
+import string
+from os.path import exists
+from os import system
+from make_tag import make_tag_with_dashes
+
+#####################################################################################################################
+
+# helper function for PDB processing
+def slice_out( inpath_dir, prefix, pdb, res_string, excise=False ):
+    starting_native = inpath_dir+'/'+pdb
+    assert( exists( starting_native ) )
+    slice_pdb = prefix + pdb
+    if not exists( slice_pdb ):
+        if excise:  command = 'pdbslice.py %s -excise %s %s ' % ( starting_native, res_string, prefix )
+        else:       command = 'pdbslice.py %s -subset %s %s ' % ( starting_native, res_string, prefix )
+        system( command )
+    assert( exists( slice_pdb ) )
+    return slice_pdb
+
+#####################################################################################################################
+
+def get_fullmodel_number( reschain, resnums, chains):
+    for m in range( len( resnums ) ):
+        if ( resnums[m] == reschain[0] ) and ( reschain[1] == '' or chains[m] == reschain[1] ): return m+1
+    return 0
+
+#####################################################################################################################
+
+def get_align_res( screen_pdb, working_pdb, working_fixed_res ):
+    from read_pdb import read_pdb
+    screen_align_res = []
+    working_align_res = []
+    for pdb in [ screen_pdb, working_pdb ]: assert( exists( pdb ) )
+    #( coords, pdb_lines, sequence, chains, residues ) = read_pdb( pdb )
+    screen_pdb_info = read_pdb( screen_pdb )
+    working_pdb_info = read_pdb( working_pdb )
+    for n in xrange( len( screen_pdb_info[4] ) ):
+        screen_chain = screen_pdb_info[3][n]
+        screen_res = screen_pdb_info[4][n]
+        #working_reschain = pdb2pose( working_pdb_info[3], working_pdb_info[4], screen_chain, screen_res )   
+        working_reschain = ( 0, '' )
+        for m in xrange( len( working_pdb_info[3] ) ):
+            if ( working_pdb_info[4][m] == screen_res ) and ( working_pdb_info[3][m] == screen_chain ): 
+                working_reschain = ( working_pdb_info[4][m], working_pdb_info[3][m] )
+        if ( working_reschain[0] > 0 ):# and ( working_reschain[0] in working_fixed_res ):
+            screen_align_res.append( n )
+            working_align_res.append( get_fullmodel_number(working_reschain,working_pdb_info[4],working_pdb_info[3]) ) 
+    if len( screen_align_res ): screen_align_res_tag = make_tag_with_dashes( screen_align_res )
+    else:   screen_align_res_tag = '0-0'
+    if len( working_align_res ):    working_align_res_tag = make_tag_with_dashes( working_align_res )
+    else:   working_align_res_tag = '0-0'
+    return ( screen_align_res_tag, working_align_res_tag )
