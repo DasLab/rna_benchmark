ff47fd91faa6b69db09af6b485d4371c1893e1e9
Rhiju Das <rhiju@stanford.edu>
2014-12-05 14:27:28 -0800

    checking in rna_res_level_energy4 to allow caleb to check favorites...

---
+++ ref/rna_res_level_energy4/r2_4x4/qsub_files/qsub9.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -N _biox3_scratch_users_rhiju_projects_stepwise_benchmark_new_rna_res_level_energy4_RPT3_r2_4x4_SWM_9
+#PBS -o /dev/null
+#PBS -e /dev/null
+#PBS -m n
+#PBS -M nobody@stanford.edu
+#PBS -l walltime=48:00:00
+
+cd /biox3/scratch/users/rhiju/projects/stepwise/benchmark/new/rna_res_level_energy4_RPT3/r2_4x4
+
+/home/rhiju/src/rosetta//main/source/bin/stepwise -s r2_4x4_HELIX1.pdb r2_4x4_HELIX2.pdb -fasta r2_4x4.fasta -terminal_res A:2 A:9 B:13 B:20 -extra_min_res A:3 A:8 B:14 B:19 -superimpose_over_all -cycles 200 -nstruct 20 -intermolecular_frequency 0.0 -save_times -native r2_4x4_2l8f_RNA.pdb -score:weights stepwise/rna/rna_res_level_energy4.wts -cycles 200 -out:file:silent SWM/9/swm_rebuild.out > /dev/null 2> /dev/null 
--- ref/rna_res_level_energy_rnatorsion1_2kcycles_FAVORITES2/anticodon/qsub_files/qsub8.sh
+++ ref/rna_res_level_energy_rnatorsion1_2kcycles_FAVORITES2/anticodon/qsub_files/qsub8.sh
@@ -4,9 +4,8 @@
 #PBS -e /dev/null
 #PBS -m n
 #PBS -M nobody@stanford.edu
-#PBS -l mem=500Mb
 #PBS -l walltime=48:00:00
 
 cd /biox3/scratch/users/rhiju/projects/stepwise/benchmark/ref/rna_res_level_energy_rnatorsion1_2kcycles_FAVORITES2/anticodon
 
-/home/rhiju/src/rosetta//main/source/bin/stepwise -s anticodon_HELIX1.pdb -fasta anticodon.fasta -terminal_res A:29 A:41 -extra_min_res A:31 A:39 -superimpose_over_all -cycles 200 -nstruct 20 -intermolecular_frequency 0.0 -save_times -native anticodon_3l0u_RNA.pdb -score:rna_torsion_potential RNA11_based_new -chemical::enlarge_H_lj -set_weights rna_torsion 1.0 -cycles 2000 -out:file:silent SWM/8/swm_rebuild.out > /dev/null 2> /dev/null 
+/home/rhiju/src/rosetta//main/source/bin/stepwise -s anticodon_HELIX1.pdb -fasta anticodon.fasta -terminal_res A:29 A:41 -extra_min_res A:31 A:39 -superimpose_over_all -cycles 200 -nstruct 20 -intermolecular_frequency 0.0 -save_times -native anticodon_3l0u_RNA.pdb -score:rna_torsion_potential RNA11_based_new -chemical::enlarge_H_lj -set_weights rna_torsion 1.0 -allow_skip_bulge -cycles 2000 -out:file:silent SWM/8/swm_rebuild.out > /dev/null 2> /dev/null 
