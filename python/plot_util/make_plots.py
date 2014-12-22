990033a45ba0f2382a0cab0741729e374ea5618e
Rhiju Das <rhiju@stanford.edu>
2014-12-22 10:19:16 -0800

    update to make_plots to get working on rhiju's directory structure. show_times does not print anything for runs with empty times.

--- python/plot_util/make_plots.py
+++ python/plot_util/make_plots.py
@@ -14,20 +14,22 @@ from make_plots_util import *
 
 ##########################################################
 
-def make_plots( inpaths, outfilename='swm_rebuild.out', target_files=['favorites.txt','favorites2.txt'], colorcode=None, xvar='rms_fill', yvar='score', scale=False, show=False, landscape=False ):
-	
+def make_plots( inpaths, outfilenames=['swm_rebuild.out','swm_rebuild.sc'], target_files=['favorites.txt','favorites2.txt'], colorcode=None, xvar='rms_fill', yvar='score', scale=False, show=False, landscape=False ):
+
 	data = []
 	which_target = []
 	outfiles_list = []
-	
+
 	if not colorcode: colorcode = [ (0.0, 0.0, 0.0, 1.0), (1.0, 0.0, 0.0, 1.0) ]
 	if len( colorcode ) < len( inpaths ): colorcode = jet( len( inpaths ) )
 	target_names = get_target_names( target_files )
 	inpaths = map( lambda x: abspath(x), inpaths )
-	
+
 	for n in xrange( len(inpaths) ):
 		assert( exists( inpaths[n] ) )
-		outfiles = popen( 'ls -1 '+inpaths[n]+'/*/'+outfilename ).read().split('\n')[:-1]
+                for outfilename in outfilenames:
+                        outfiles = popen( 'ls -1 '+inpaths[n]+'/*/'+outfilename ).read().split('\n')[:-1]
+                        if len( outfiles ) > 0: break
 		for outfile in outfiles:
 			print 'Reading in ... '+outfile
 			assert( exists( outfile ) )
@@ -37,7 +39,7 @@ def make_plots( inpaths, outfilename='swm_rebuild.out', target_files=['favorites
 	noutfiles = np.max( map( lambda x: len(x), outfiles_list ) )
 
 	###################################################
-	
+
 	# print out runtimes, stored in the silent files
 	show_times( inpaths, data, noutfiles, target_names, which_target )
 
@@ -57,9 +59,9 @@ def make_plots( inpaths, outfilename='swm_rebuild.out', target_files=['favorites
 
 	# setup pdf file name, and PdfPages handle
 	pdfname = basename( inpaths[0] )
-	if len( inpaths ) > 1:	
+	if len( inpaths ) > 1:
 		for k in xrange( 1, len( inpaths ) ): pdfname += '_vs_' + basename( inpaths[k] )
-	fullpdfname = get_path_to_dir('stepwise_benchmark') + '/Figures/' + pdfname # + '.pdf'
+	fullpdfname = get_path_to_dir( ['stepwise_benchmark','benchmark'] ) + '/Figures/' + pdfname # + '.pdf'
 	if landscape:	fullpdfname += '_landscape.pdf'
 	else:			fullpdfname += '.pdf'
 	print '\nMaking figure in: %s\n' % fullpdfname
@@ -78,7 +80,7 @@ def make_plots( inpaths, outfilename='swm_rebuild.out', target_files=['favorites
 
 		# iterate over targets
 		for target in target_names:
-			
+
 			# add subplot, if scores are available
 			plot_idx += 1
 			try: data[n][ target ]
@@ -90,7 +92,7 @@ def make_plots( inpaths, outfilename='swm_rebuild.out', target_files=['favorites
 			[ xvar_data, yvar_data ] = [ list(d) for d in zip( *[ ( score[xvar_idx], score[yvar_idx] ) for score in data[n][ target ].scores] ) ]
 
 			# plot data
-			ax.plot( xvar_data, yvar_data, marker='.', markersize=5, color=colorcode[n], linestyle=' ', label=basename(inpaths[n]) )	
+			ax.plot( xvar_data, yvar_data, marker='.', markersize=5, color=colorcode[n], linestyle=' ', label=basename(inpaths[n]) )
 			ax.plot( [1 for y in plt.ylim()], plt.ylim(), color='black', linestyle=':')
 			ax.plot( [2 for y in plt.ylim()], plt.ylim(), color='black')
 
@@ -100,10 +102,10 @@ def make_plots( inpaths, outfilename='swm_rebuild.out', target_files=['favorites
 			# set title and axes lables
 			if landscape:	ax.set_title( get_title(target), fontsize='small', weight='bold' )
 			else:			ax.set_title( get_title(target), fontsize='medium', weight='bold' )
-			if ( ( np.mod( plot_idx, ncols ) == 1 ) or ( ncols == 1 ) ):	
+			if ( ( np.mod( plot_idx, ncols ) == 1 ) or ( ncols == 1 ) ):
 				if landscape:	ax.set_ylabel( yvar, fontsize='small' )
 				else:			ax.set_ylabel( yvar, fontsize='medium' )
-			if ( ( np.floor( (plot_idx-1) / ncols ) == nrows-1 ) or ( nrows == 1 ) ):	
+			if ( ( np.floor( (plot_idx-1) / ncols ) == nrows-1 ) or ( nrows == 1 ) ):
 				if landscape:	ax.set_xlabel( xvar, fontsize='small' )
 				else:			ax.set_xlabel( xvar, fontsize='medium' )
 
@@ -112,7 +114,7 @@ def make_plots( inpaths, outfilename='swm_rebuild.out', target_files=['favorites
 			for tick in ax.yaxis.get_ticklabels():	tick.set_fontsize(6)
 
 			# setup legend
-			if ( plot_idx == 3 ):	
+			if ( plot_idx == 3 ):
 				legend = ax.legend(shadow=True)
 				for label in legend.get_texts():	label.set_fontsize(8)
 				for label in legend.get_lines():	label.set_linewidth(.5)
@@ -136,8 +138,8 @@ if __name__=='__main__':
 	import argparse
 
 	parser = argparse.ArgumentParser(description='Make plots of scores from silent files.')
-	parser.add_argument('inpaths', nargs='+', help='List of paths to silent files.')
-	parser.add_argument('-outfilename', help='Name of silent file.', default='swm_rebuild.out')
+	parser.add_argument('inpaths', nargs='+', help='List of paths too silent files.')
+	parser.add_argument('-outfilename', nargs='*', help='Name of silent file.', default=['swm_rebuild.out','swm_rebuild.sc'])
 	parser.add_argument('-target_files', nargs='+', help='List of additional target files.', default=['favorites.txt','favorites2.txt'])
 	parser.add_argument('-xvar', help='Name of x variable.', default='rms_fill')
 	parser.add_argument('-yvar', help='Name of y variable.', default='score')
@@ -146,4 +148,4 @@ if __name__=='__main__':
 	parser.add_argument('--show', help='show plot after it is created.', action='store_true')
 	args=parser.parse_args()
 
-	make_plots( args.inpaths, outfilename=args.outfilename, target_files=args.target_files, xvar=args.xvar, yvar=args.yvar, scale=args.scale, show=args.show, landscape=args.landscape )
+	make_plots( args.inpaths, outfilenames=args.outfilename, target_files=args.target_files, xvar=args.xvar, yvar=args.yvar, scale=args.scale, show=args.show, landscape=args.landscape )
--- python/plot_util/make_plots_util.py
+++ python/plot_util/make_plots_util.py
@@ -13,7 +13,7 @@ import numpy as np
 ##########################################################
 
 class ScoreData(object):
-	
+
 	def __init__(self):
 		self.scores = None
 		self.score_labels = None
@@ -28,11 +28,11 @@ def load_score_data( file ):
 		if len( line ) < 2: return []
 		cols = line.split()
 		if len( cols ) > 0 and cols[0] == 'SCORE:':
-			score_labels = cols[1:len( cols )-1] 
+			score_labels = cols[1:len( cols )-1]
 			break
 
 	keyword = 'SCORE'
-	scorenums = [ str(x) for x in xrange( 2, len( score_labels )+2 ) ] 
+	scorenums = [ str(x) for x in xrange( 2, len( score_labels )+2 ) ]
 
 	command = 	'grep '+keyword+' '+file 								 	\
 				+ ' | awk \'{print $'+string.join( scorenums, ',$' )+'}\''  \
@@ -58,7 +58,7 @@ def load_score_data( file ):
 def get_target_names( target_files ):
 	target_names = []
 	for file_name in target_files:
-		if '..' not in file_name:	file_name = get_path_to_dir('stepwise_benchmark') + '/input_files/' + file_name
+		if '..' not in file_name:	file_name = get_path_to_dir(['stepwise_benchmark','benchmark']) + '/input_files/' + file_name
 		target_names = get_target_names_from_file( file_name, target_names )
 	return target_names
 
@@ -69,26 +69,28 @@ def get_target_names_from_file( filename, target_names ):
 	for line in fid.readlines():
 		### Old target file format
 		### Name 			Sequence 	Secstruct ...
-		### target_name 
+		### target_name
 		#if 'Name' in line: continue
 		#cols = line.split()
 		#if not len( cols ): continue
-		#target_names.append( cols[0] ) 
-		
+		#target_names.append( cols[0] )
+
 		### New target file format
 		### Name:		target_name
 		cols = string.split( line.replace( '\n', '' ) )
 		if not len( cols ): continue
-		if cols[0] == 'Name:': target_names.append( cols[1] ) 
+		if cols[0] == 'Name:': target_names.append( cols[1] )
 
 	fid.close()
 	return target_names
 
 ###########################################################
 
-def get_path_to_dir( dirname ):
-	pwd = popen( 'pwd' ).readline()[:-1].split( '/' )
-	return string.join( pwd[:pwd.index( dirname )+1], '/' )
+def get_path_to_dir( dirnames ):
+        for dirname in dirnames:
+                pwd = popen( 'pwd' ).readline()[:-1].split( '/' )
+                if dirname not in pwd: continue
+                return string.join( pwd[:pwd.index( dirname )+1], '/' )
 
 ###########################################################
 
@@ -104,17 +106,23 @@ def show_times( inpaths, data, noutfiles, target_names, which_target ):
 				time_idx = data[n][ target ].score_labels.index( time_name )
 				times.append( np.array( [ score[time_idx] for score in data[n][ target ].scores ], dtype = 'float_' ) )
 			except:
-				times.append( np.array( [ 0.0 for score in xrange( noutfiles ) ], dtype = 'float_' ) )
+				times.append( [] )
+                                #times.append( np.array( [ 0.0 for score in xrange( noutfiles ) ], dtype = 'float_' ) )
 		times_list.append( times )
 
 
 	for k in xrange( len(target_names) ):
+                times_found = False
+		for n in xrange( len( inpaths ) ):
+                        if ( len( times_list[n][k] ) > 0.0 ): times_found = True
+                if not times_found: continue
+
 		print '\n %-6s%33s' % ( 'TARGET', target_names[ k ] ) #which_target[0][k] ] )
 		for n in xrange( len( inpaths ) ):
 			mean_time = np.mean( times_list[n][k] )
 			std_time = np.std( times_list[n][k] )
 			print ' Run %d                    %5.0f +/- %4.0f' % ( n, mean_time, std_time )
-	
+
 	print '\n'
 	for n in xrange( len( inpaths ) ):
 		print ' Run %d: %s' % ( n, basename( inpaths[n] ) )
@@ -133,12 +141,12 @@ def jet( size ):
 ###########################################################
 
 def get_title( target ):
-	names = get_path_to_dir('stepwise_benchmark') + '/python/plot_util/titles.txt'
+	names = get_path_to_dir(['stepwise_benchmark','benchmark']) + '/python/plot_util/titles.txt'
 	try:
 		lines = open( names, 'r' ).readlines()
 	except:
 		return target
 	for line in lines:
-		if target in line: 
+		if target in line:
 			return line.replace('\n','').split(': ')[1]
 	return target
