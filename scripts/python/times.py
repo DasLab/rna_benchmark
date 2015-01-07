8b001d0054f6e23d41b64692d92ae2a88a209703
calebgeniesse <calebgeniesse@gmail.com>
2015-01-07 09:43:57 -0800

    Minor updates to times.py for getting SWA run times.

--- scripts/python/times.py
+++ scripts/python/times.py
@@ -1,23 +1,29 @@
-#!/usr/env/python
+#!/usr/bin/python
 
 from os import popen
-command = "grep 'seconds' KEEP_LOG_FILE/CONDOR/*/*/*/*.out"
+from sys import argv
 
-lines = popen( command ).readlines()
+targets = []
+for arg in argv[1:]:
+	targets.append( arg )
 
-total_time = 0
-for line in lines:
-	cols = line.split()
-	for col in cols:
-		if '.' not in col: continue 
-		try:
-			time = float(col)
+for target in targets:
+	print '\nGetting times for %s' % target
+	command = "grep 'seconds' %s/KEEP_LOG_FILE/CONDOR/*/*/*/*.out" % target
+	lines = popen( command ).readlines()
+	total_time = 0.0
+	for line in lines:
+		cols = line.split()
+		for idx, col in enumerate(cols):
+			if 'seconds' not in col:
+				continue
+			try:
+				time = float(cols[idx-1])
+			except:
+				continue
 			total_time+=time
-		except:
-			continue
-		#print col
 
-print 'Total Time: ', total_time, ' seconds ' 
-print 'Total Time: ', (total_time/60.), ' minutes ' 
-print 'Total Time: ', ((total_time/60.)/60.), ' hours ' 
-print 
+	print 'Total Time: ', total_time, ' seconds '
+	print 'Total Time: ', (total_time/60.), ' minutes '
+	print 'Total Time: ', ((total_time/60.)/60.), ' hours '
+
