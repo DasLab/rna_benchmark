1b56fb08f5efe53d1a37b4dd550af6bb805f28f7
calebgeniesse <calebgeniesse@gmail.com>
2015-01-07 10:43:25 -0800

    Refactoring times.py to look in LOG_DEBUG_QSTAT_FULL.txt for cput and walltime.

--- scripts/python/times.py
+++ scripts/python/times.py
@@ -3,27 +3,24 @@
 from os import popen
 from sys import argv
 
-targets = []
-for arg in argv[1:]:
-	targets.append( arg )
+if len( argv ) > 1:
+	targets = argv[1:]	
+else:
+	targets = ["."]
 
 for target in targets:
-	print '\nGetting times for %s' % target
-	command = "grep 'seconds' %s/KEEP_LOG_FILE/CONDOR/*/*/*/*.out" % target
-	lines = popen( command ).readlines()
-	total_time = 0.0
-	for line in lines:
-		cols = line.split()
-		for idx, col in enumerate(cols):
-			if 'seconds' not in col:
-				continue
-			try:
-				time = float(cols[idx-1])
-			except:
-				continue
-			total_time+=time
+	print '\n%s' % target
+	for time_key in ['resources_used.cput', 'resources_used.walltime']:	
+		command = "grep %s %s/LOG_DEBUG_QSTAT_FULL.txt" % (time_key,target)
+		lines = popen( command ).readlines()
+		total_seconds = 0.0
+		for line in lines:
+			if time_key not in line or '=' not in line: continue
+			[hh,mm,ss] = line.split()[2].split(':')
+			total_seconds += float(hh)*60.*60. + float(mm)*60. + float(ss)
+		mm, ss = divmod(total_seconds, 60)
+		hh, mm = divmod(mm, 60)
+		time_string = "%d:%02d:%02d" % (hh, mm, ss)
+		print '%-23s = %s' % (time_key, time_string)	
 
-	print 'Total Time: ', total_time, ' seconds '
-	print 'Total Time: ', (total_time/60.), ' minutes '
-	print 'Total Time: ', ((total_time/60.)/60.), ' hours '
 
