09b5c623f68bcdecea60fc4d308be25488edf199
calebgeniesse <calebgeniesse@gmail.com>
2015-06-16 11:53:28 -0700

    update setup_erraser_benchmark.py to run on comet

--- scripts/python/benchmark_util/setup_erraser_benchmark.py
+++ scripts/python/benchmark_util/setup_erraser_benchmark.py
@@ -151,6 +151,7 @@ qsub_file = 'qsubMINI'
 hostname, hostname_err = subprocess.Popen(['hostname'], stdout=subprocess.PIPE).communicate()
 if hostname.find( 'stampede' ) > -1: qsub_file = 'qsubMPI'
 if hostname.find( 'sherlock' ) > -1: qsub_file = 'sbatchMINI'
+if hostname.find( 'comet' ) > -1: qsub_file = 'sbatchMINI'
 fid_qsub = open( qsub_file, 'w' )
 
 for name in names:
