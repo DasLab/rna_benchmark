3b3b4c6b3bd157b738b345198ab700e3ac26a15c
Rhiju Das <rhiju@stanford.edu>
2016-07-13 09:24:16 -0700

    checking in metal ion runs into ref/ -- will provide an example in SWM paper

---
+++ ref/metal_ions_test/azo_groupI_product/qsub_files/qsub6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -N _Users_rhiju_Dropbox_projects_stepwise_benchmark_new_metal_ions_test_azo_groupI_product_SWM_6
+#PBS -o /dev/null
+#PBS -e /dev/null
+#PBS -m n
+#PBS -M nobody@stanford.edu
+#PBS -l walltime=48:00:00
+
+cd /Users/rhiju/Dropbox/projects/stepwise/benchmark/new/metal_ions_test/azo_groupI_product
+
+/Users/rhiju/src/rosetta//main/source/bin/stepwise -s azo_groupI_product_START1_3BO3_altA_renumber.pdb -native azo_groupI_product_NATIVE_3BO3_altA_renumber.pdb -terminal_res B:8 B:12 B:127 B:137 B:169 B:179 D:1 -block_stack_above_res B:12 B:137 B:179 -block_stack_below_res B:8 B:127 B:169 D:1 -extra_min_res D:2 -fasta azo_groupI_product.fasta -nstruct 20 -save_times -score:weights stepwise/rna/rna_res_level_energy4.wts -set_weights mg_lig 1.0 mg_sol 0.2 hoh_ref 1.0 -cycles 2000 -motif_mode -out:file:silent SWM/6/swm_rebuild.out > 6.out 2> 6.err 
---
+++ ref/metal_ions_test/hcv_ires_IIa/qsub_files/qsub5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -N _Users_rhiju_Dropbox_projects_stepwise_benchmark_new_metal_ions_test_hcv_ires_IIa_SWM_5
+#PBS -o /dev/null
+#PBS -e /dev/null
+#PBS -m n
+#PBS -M nobody@stanford.edu
+#PBS -l walltime=48:00:00
+
+cd /Users/rhiju/Dropbox/projects/stepwise/benchmark/new/metal_ions_test/hcv_ires_IIa
+
+/Users/rhiju/src/rosetta//main/source/bin/stepwise -s hcv_ires_IIa_START1_2PN3.pdb -native hcv_ires_IIa_NATIVE_2PN3.pdb -terminal_res A:51 A:59 B:109 B:112 -block_stack_above_res A:59 B:112 -block_stack_below_res A:51 B:109 -extra_min_res A:52 A:58 -fasta hcv_ires_IIa.fasta -nstruct 20 -save_times -score:weights stepwise/rna/rna_res_level_energy4.wts -set_weights mg_lig 1.0 mg_sol 0.2 hoh_ref 1.0 -cycles 2000 -motif_mode -out:file:silent SWM/5/swm_rebuild.out > 5.out 2> 5.err 
