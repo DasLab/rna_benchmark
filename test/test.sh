5c91cab16449aa6251c7ec41c4dd1149f3d553fc
Rhiju Das <rhiju@stanford.edu>
2016-12-12 20:21:59 -0800

    user colordiff to output diffs if available.

--- test/test.sh
+++ test/test.sh
@@ -11,10 +11,17 @@ if [[ -d ref ]] ; then
 	done
 	cd ..
 	diff -r -u -I '#PBS.*' -I '/Users/amw579/nucleic/stepwise_benchmark/test' -x '*.sh' -x 'qsubMINI' -x 'bsubMINI' -x 'sbatchMINI' -x 'sbatchMPI' -x 'condorMINI' -x 'MPI_ONEBATCH.job' -x 'sbatch_files' -x 'sbatch_files_MPI' -x 'sbatchMPI_ONEBATCH' -x 'README_SWM' --exclude="*/qsub_files" ref new > tempdiff
+	
 	if [[ -s tempdiff ]]; then
-		echo "Directories were meaningfully different; do not commit these changes until you are certain the diff is correct."
+	    if type "colordiff" >& /dev/null; then
+		cat tempdiff | colordiff
+	    else
+		cat tempdiff
+		echo "(If you install colordiff, the diffs above will look pretty.)"
+	    fi
+	    echo "Directories were meaningfully different; do not commit these changes until you are certain the diff is correct."
 	else
-		echo "Clean diff; go ahead and commit."
+	    echo "Clean diff; go ahead and commit."
 	fi
 else
 	# generate ref
