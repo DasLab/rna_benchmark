466cc7acf48eef6000aca9ba61ece007df2a0bfb
Andy Watkins <andy.watkins2@gmail.com>
2017-04-05 18:33:19 -0700

    alignment?

--- scripts/python/make_md.py
+++ scripts/python/make_md.py
@@ -7,7 +7,7 @@ with open("README.md", "w") as out:
 	out.write("<table>\n")
 	for i, native_pdb in enumerate(glob.glob("*NATIVE*.pdb")):
 		#native_pdb = re.sub("\_", "\\\_", native_pdb)
-		data_str = "<td text-align=\"center\">%s<br /><img src=%s /></td>" % (re.sub("_NATIVE.*", "", native_pdb), re.sub(".pdb", ".png", native_pdb))
+		data_str = "<td align=\"center\">%s<br /><img src=%s /></td>" % (re.sub("_NATIVE.*", "", native_pdb), re.sub(".pdb", ".png", native_pdb))
 		if i % 3 == 0:
 			out.write( "<tr>%s" % data_str )
 		elif i % 3 == 1:
